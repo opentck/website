118e75e0c712918a41cbdb1297075b30ebed00ea
Dominik Dorn <dominik@dominikdorn.com>
2010-12-15 17:49:31 +0100

    security controller

---
+++ app/controllers/Security.java
@@ -0,0 +1,41 @@
+package controllers;
+
+import models.User;
+
+/**
+ * Dominik Dorn
+ * 0626165
+ * dominik.dorn@tuwien.ac.at
+ */
+public class Security extends Secure.Security {
+
+    public static void login() throws Throwable {
+        redirect("/auth_twitter");
+    }
+
+    static boolean authenticate(String username, String password)
+    {
+        return false;
+    }
+
+    static void onDisconnected() {
+        Application.index();
+    }
+
+    static void onAuthenticated() {
+        Admin.index();
+    }
+
+    static boolean check(String profile) {
+        if(connected() == null)
+            return false;
+        if("admin".equals(profile)) {
+            User u = User.find("username", connected()).first();
+            if(u == null)
+                return false;
+            return u.isAdmin;
+        }
+    return false;
+}
+
+}
